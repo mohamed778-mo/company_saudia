eb820338ec2debead919dd7c1563804fa7716b9c
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-18 00:17:49 +0300

    Update video_router.js

--- routers/video_router.js
+++ routers/video_router.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const router=express.Router()
 const { getVideoinService, deletevideo, postvideofromviemotoDB}=require('../controllers/video_control')
-const {adminAuth}=require("../middleware/auth")
+const { adminAuth } = require('../middleware/auth');
 
 
-
-router.get('/get_video_in_service/:service_id',adminAuth,getVideoinService)
-router.delete('/delete_video/:service_id/:video_id',adminAuth,deletevideo)
-router.put('/download_video/:service_id',adminAuth,postvideofromviemotoDB)
+router.get('/get_video_in_service/:service_id', adminAuth , getVideoinService)
+router.delete('/delete_video/:service_id/:video_id', adminAuth ,deletevideo)
+router.post('/download_video/:service_id', adminAuth , postvideofromviemotoDB)
 
 
 
