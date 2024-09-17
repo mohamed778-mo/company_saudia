0697dbeaa339db340787c817b5a595b4c223df78
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-18 01:04:58 +0300

    Update user_router.js

--- routers/user_router.js
+++ routers/user_router.js
@@ -10,7 +10,7 @@ const {  get_service , get_all_services ,get_main, get_all_main } = require('../
 
 router.post('/create_form', setLanguage, create_form);
 
-router.get('/get_main',setLanguage, get_main);
+router.get('/get_main/:main_id',setLanguage, get_main);
 router.get('/get_all_main',setLanguage, get_all_main);
 
 router.get('/get_service/:sevice_id',setLanguage, get_service);
