ecb1a60949caa200f5cf712e3674b526c8a57d9a
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-17 22:04:47 +0300

    Update user_router.js

--- routers/user_router.js
+++ routers/user_router.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 ;
 const { setLanguage } = require('../middleware/setLanguage');     
 
-const {  get_service , get_all_services } = require('../controllers/dashboard_control');
+const {  get_service , get_all_services ,get_main, get_all_main } = require('../controllers/dashboard_control');
 
    const { create_form }=require('../controllers/web_control')
 
 
 
 router.post('/create_form', setLanguage, create_form);
+
+router.get('/get_main',setLanguage, get_main);
+router.get('/get_all_main',setLanguage, get_all_main);
 
 router.get('/get_service/:sevice_id',setLanguage, get_service);
 router.get('/get_all_service/:main_id',setLanguage, get_all_services);
