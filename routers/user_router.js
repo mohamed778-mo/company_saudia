95d9e619801868636b0814186fc039063ab0e0f3
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-20 22:32:09 +0300

    Update user_router.js

--- routers/user_router.js
+++ routers/user_router.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 ;
 const { setLanguage } = require('../middleware/setLanguage');     
 
-const {  get_service , get_all_services ,get_main, get_all_main } = require('../controllers/dashboard_control');
+const {  get_service , get_all_services ,get_main, get_all_main  } = require('../controllers/dashboard_control');
 
-   const { create_form }=require('../controllers/web_control')
+   const { create_form , create_contact_form }=require('../controllers/web_control')
 
 
 
 router.post('/create_form/:service_id', setLanguage, create_form);
+router.post('/create_contact_form', setLanguage, create_contact_form);
+
 
 router.get('/get_main/:main_id',setLanguage, get_main);
 router.get('/get_all_main',setLanguage, get_all_main);
