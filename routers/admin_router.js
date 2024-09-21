e7fd055b2c3c4d50b977d00d62b7463d2f93f127
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-21 16:34:00 +0300

    Update admin_router.js

--- routers/admin_router.js
+++ routers/admin_router.js
@@ -54,7 +54,7 @@ router.delete('/delete_all_main',adminAuth, delete_all_mains);
 
 router.post('/add_service_to_main/:main_id',adminAuth, Iupload.any() , add_service_to_main);
 
-router.patch('/edit_service/:service_id', adminAuth,  Iupload.any() ,edit_service);
+router.patch('/edit_service/:service_id', adminAuth,  Iupload.single('file') ,edit_service);
 
 
 router.get('/get_service_in_dash/:service_id',adminAuth,  get_service_in_dash);
