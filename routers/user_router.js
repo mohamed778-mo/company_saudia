5a3af84b1e7e81bdfaf392e348a8d85acb954e68
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-22 23:10:48 +0300

    Update user_router.js

--- routers/user_router.js
+++ routers/user_router.js
@@ -4,12 +4,13 @@ const { setLanguage } = require('../middleware/setLanguage');
 
 const {  get_service , get_all_services ,get_main, get_all_main  } = require('../controllers/dashboard_control');
 
-   const { create_form , create_contact_form , createformother}=require('../controllers/web_control')
+   const { create_form , create_contact_form , createformother , create_form_special }=require('../controllers/web_control')
 
 
 
 router.post('/create_form/:service_id', setLanguage, create_form);
 router.post('/CreateFormOther', setLanguage, createformother);
+router.post('/create_form_special', setLanguage, create_form_special);
 
 router.post('/create_contact_form', setLanguage, create_contact_form);
 
